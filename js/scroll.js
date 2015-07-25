73dfff3b57a5bba5446f514aed7d8513034c2c03
Alok Jain <alokjain@xhtml-lab.com>
2015-07-25 17:56:20 +0530

    Working on reverse animation code.

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
 
-  //Define variables
+  //Configurable variables
+  var blocks = 4;                       //Total number of blocks
+  var $mainBlock = $('.main-block');    //Wrapper element of all blocks
+  var $block = $('.main-block .block'); //Blocks
+  var lastBlockOn = true;               //Keep last block highlighted
+
+  //Other variables used for functionality
   var windowHeight = $(window).height();
-  var blockHeight = windowHeight / 4;
-  var $mainBlock = $('.main-block');
-  var $block = $('.main-block .block');
+  var blockHeight = windowHeight / blocks;
   var currentBlock = 0;
   var checkPoint = blockHeight;
+
+  //Keep last block highlighted or not?
+  if (lastBlockOn) { blocks--; }
 
   //Initial Setup for height and positioning etc.
   $mainBlock.css({'position': 'fixed'});
@@ -15,8 +22,8 @@ $(document).ready(function() {
   $block.first().addClass('active');
 
   //Functionality on page scroll
-  $(document).on("scroll", function() {
-    var scrollPos = $(window).scrollTop();
+  $(window).on("scroll", function() {
+    var scrollPos = $(this).scrollTop();
 
     /* Check if we have scrolled equal to browser height,
        and then remove positioning to make page behave normally. */
@@ -25,13 +32,28 @@ $(document).ready(function() {
       $('body').css({'padding-top': '0px'}).scrollTop(0);
     }
 
+    // Reverse animation
+    /* Check if we have reached at top, and we have not already changed position
+       Then change position of mainBlock to be fixed and add other settings */
+    if (scrollPos == 0 && currentBlock > 0) {
+      if( $mainBlock.css('position').toLowerCase() == 'static') {
+        $mainBlock.css({'position': 'fixed'});
+        $('body').css({'padding-top': windowHeight * 2}).scrollTop(windowHeight);
+      }
+    }
+
     //Check scroll position, and activate blocks accordingly
-    if(scrollPos >= checkPoint && currentBlock < 3) {
+    if(scrollPos >= checkPoint && currentBlock < blocks) {
       currentBlock++;
       checkPoint = blockHeight * (currentBlock + 1);
       $block.removeClass('active');
       $block.eq(currentBlock).addClass('active');
-    }
+    } /*else if(scrollPos <= checkPoint && currentBlock >= 0) {
+      currentBlock--;
+      checkPoint = blockHeight * (currentBlock + 1);
+      $block.removeClass('active');
+      $block.eq(currentBlock).addClass('active');
+    }*/
 
   });//end of scroll event function
 
